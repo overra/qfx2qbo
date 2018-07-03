faa725e8c28993d05f41791dae95cfec657f65f6
Adam Snodgrass <overra@gmail.com>
2018-07-03 09:00:39 -0500

    :sparkles: add sw-precache-webpack-plugin

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const webpack = require("webpack");
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const PUBLIC_PATH = "https://convert-qfx.now.sh/";
 
 module.exports = {
   webpack: (config, { dev }) => {
@@ -9,6 +11,20 @@ module.exports = {
         "process.env.ALGOLIA_KEY": JSON.stringify(process.env.ALGOLIA_KEY)
       })
     );
+
+    if (!dev) {
+      config.plugins.push(
+        new SWPrecacheWebpackPlugin({
+          cacheId: "convert-qfx",
+          dontCacheBustUrlsMatching: /\.\w{8}\./,
+          filename: "service-worker.js",
+          minify: true,
+          navigateFallback: PUBLIC_PATH + "index.html",
+          staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/]
+        })
+      );
+      config.output.publicPath = PUBLIC_PATH;
+    }
     return config;
   }
 };
