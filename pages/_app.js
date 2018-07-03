257e5a8ef2dd18b2a02b633de91b841aa01fcfdd
Adam Snodgrass <overra@gmail.com>
2018-07-03 09:05:36 -0500

    :sparkles: register service worker

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,12 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import JssProvider from "react-jss/lib/JssProvider";
 import getPageContext from "../src/getPageContext";
 
+(function() {
+  if (navigator && "serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/service-worker.js");
+  }
+})();
+
 class MyApp extends App {
   constructor(props) {
     super(props);
